e1e56b801bf91ed41440eca9ac63c6034f081bef
yannviel <elv-yann.vllrd@eduge.ch>
2021-02-20 14:55:03 +0100

    supression d'une erreu dasn la fonction afficher recap et ajout des commentaires

--- JS/BonCadeau.js
+++ JS/BonCadeau.js
@@ -1,4 +1,5 @@
 
+//création de la map source : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Map
 var mapPrixDuree = new Map();
 select = document.getElementById('PrixDuree');
 
@@ -6,7 +7,7 @@ mapPrixDuree.set("1h","130 CHF");
 mapPrixDuree.set("1h15","160 CHF");
 mapPrixDuree.set("1h30","190 CHF");
 
-
+// boucle permettant de récupéréer les valeurs de la map et les afficher dans le select source : https://stackoverflow.com/questions/55921971/how-to-get-a-map-array-in-javascript-to-print-in-a-dropdown-list
 let str = '<option>sélectionner une durée</option>';
 
 mapPrixDuree.forEach((key, val) => {
@@ -16,11 +17,7 @@ mapPrixDuree.forEach((key, val) => {
 select.innerHTML = str;
 
 
-
-var selectPrix = document.getElementById('PrixDuree')
-selectPrix.addEventListener('change', afficherRecap)
-
-
+// fonction qui crée une div recap en fonction de la valeur sélectionner dans le select
 function afficherRecap() {
     var divRecap = document.getElementById('recap')
     var selection = document.getElementById('PrixDuree')
@@ -28,7 +25,9 @@ function afficherRecap() {
     var prix = selection.options[selection.selectedIndex].value
 
     divRecap.innerHTML = '<h4>Récapitulatif de la commande :</h4>' +
-        'qaw4'+
         '<p>'+'Durée : '+texte+'</p>' +
         '<p>'+'Prix : '+prix+'</p>'
 }
+// affichage de la div source : Gonzalo
+var selectPrix = document.getElementById('PrixDuree')
+selectPrix.addEventListener('change', afficherRecap)
